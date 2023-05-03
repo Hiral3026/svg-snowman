aa982870afc60a09cb85dfface3dafcf19976e28
Hiral Patel <hiral@hiralpatel3026@gmail.com>
2023-05-03 00:49:50 -0400

    Initial commit

---
+++ Lab-12-svg.js
@@ -0,0 +1,12 @@
+//LAB 12 - SVG WINTER SCENE
+const nose = document.querySelector('#nose');
+
+  nose.addEventListener('click', function() {
+    nose.setAttribute('fill', 'red');
+  });
+
+
+
+
+
+
